6b594a6f50f241c418c09513e1ec68dfc07b85f4
Paul Evans <paulevans89@gmail.com>
2020-03-23 04:16:26 +0000

    Updates

---
+++ precache-manifest.c61a6360ff45b5f9ce39077d0295b470.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833f6fa12388310fce8887528c82b382",
+    "url": "/spotify-playlist-maker/index.html"
+  },
+  {
+    "revision": "3a40bc0d044ff0ceaff6",
+    "url": "/spotify-playlist-maker/static/css/main.34ca02bf.chunk.css"
+  },
+  {
+    "revision": "ae181a1ef693d2b485fd",
+    "url": "/spotify-playlist-maker/static/js/2.8e1baa02.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/spotify-playlist-maker/static/js/2.8e1baa02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a40bc0d044ff0ceaff6",
+    "url": "/spotify-playlist-maker/static/js/main.73eaca1b.chunk.js"
+  },
+  {
+    "revision": "4a0bb9170c230a4c00e6",
+    "url": "/spotify-playlist-maker/static/js/runtime-main.9e9a2a25.js"
+  },
+  {
+    "revision": "72b6f2c9990bd3d37b8013a59ed78902",
+    "url": "/spotify-playlist-maker/static/media/ProximaNova-Bold.72b6f2c9.otf"
+  },
+  {
+    "revision": "bf9f5d50c1b928ff21436517a1a95ad9",
+    "url": "/spotify-playlist-maker/static/media/ProximaNova-Regular.bf9f5d50.otf"
+  }
+]);
